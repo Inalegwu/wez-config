4245b420cedfcbf42bf00d20872f7dfbaf64d1f0
Ikwue Inalegwu <ikwueinalegwu@gmail.com>
2024-10-17 21:16:20 +0000

    font

--- wezterm.lua
+++ wezterm.lua
@@ -12,11 +12,11 @@ config={
   color_scheme="Poimandres",
   -- color_scheme="Catppuccin Mocha (Gogh)",
   -- font=wezterm.font("Maple Mono"),
-  -- font=wezterm.font("MonoLisa"),
+  font=wezterm.font("MonoLisa"),
   -- font=wezterm.font("Cartograph CF"),
   -- font=wezterm.font("Rec Mono Casual"),
-  font=wezterm.font("Operator Mono Lig"),
-  font_size=19,
+  -- font=wezterm.font("Operator Mono Lig"),
+  font_size=17.5,
   window_background_opacity=0.89999,
   scrollback_lines=3500
 }
